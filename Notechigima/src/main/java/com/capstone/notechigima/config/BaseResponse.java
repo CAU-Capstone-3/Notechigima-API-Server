392786275155cb5e95010636761996a5b59ef090
gimangi <khk211113@naver.com>
2022-10-27 01:07:51 +0900

    Add BaseResponse

---
+++ Notechigima/src/main/java/com/capstone/notechigima/config/BaseResponse.java
@@ -0,0 +1,28 @@
+package com.capstone.notechigima.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Getter;
+
+@Getter
+@JsonPropertyOrder({"status", "success", "message", "result"})
+public class BaseResponse<T> {
+
+    private final int status;
+
+    private final boolean success;
+
+    private final String message;
+
+    @JsonInclude(Include.NON_NULL)
+    private T result;
+
+    public BaseResponse(BaseResponseStatus responseStatus, T result) {
+        this.status = responseStatus.getStatus();
+        this.success = responseStatus.isSuccess();
+        this.message = responseStatus.getMessage();
+        this.result = result;
+    }
+
+}
---
+++ Notechigima/src/main/java/com/capstone/notechigima/config/BaseResponseStatus.java
@@ -0,0 +1,18 @@
+package com.capstone.notechigima.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum BaseResponseStatus {
+
+    SUCCESS_READ(true, 200, "요청에 성공하였습니다.");
+    SUCCESS_EDIT(true, 201, "요청 처리를 완료하였습니다.");
+
+    REQUEST_ERROR(false, 404, "요청한 값이 잘못되었습니다.");
+
+    private final boolean success;
+    private final int status;
+    private final String message;
+}
