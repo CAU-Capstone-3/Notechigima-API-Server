43b564b7db9527b81fe62c368cc3cb0165276c9a
gimangi <khk211113@naver.com>
2022-10-27 00:41:59 +0900

    project init

---
+++ Notechigima/src/main/java/com/capstone3/notechigima/NotechigimaApplication.java
@@ -0,0 +1,13 @@
+package com.capstone3.notechigima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotechigimaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotechigimaApplication.class, args);
+    }
+
+}
---
+++ Notechigima/src/test/java/com/capstone3/notechigima/NotechigimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone3.notechigima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotechigimaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
