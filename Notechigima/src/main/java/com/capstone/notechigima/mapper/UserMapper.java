007353cc476b02614a87495242376f0fa46630e3
김형기 <khk211113@naver.com>
2022-11-27 05:35:16 +0900

    fix: 유저 dto 매핑 오류 수정

--- Notechigima/src/main/java/com/capstone/notechigima/mapper/UserMapper.java
+++ Notechigima/src/main/java/com/capstone/notechigima/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import com.capstone.notechigima.domain.users.User;
 import com.capstone.notechigima.dto.users.UserGetResponseDTO;
 import com.capstone.notechigima.dto.users.UserNicknameGetResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -11,5 +12,7 @@ public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     UserGetResponseDTO toUserGetResponseDTO(User entity);
+
+    @Mapping(target = "userName", source = "nickname")
     UserNicknameGetResponseDTO toUserNicknameGetResponseDTO(User entity);
 }
