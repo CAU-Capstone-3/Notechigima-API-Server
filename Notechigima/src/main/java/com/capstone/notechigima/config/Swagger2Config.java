d2c614da8d0d1a2efa8f02b5273a9c2f532b4b88
김형기 <khk211113@naver.com>
2022-11-29 02:05:01 +0900

    docs: 스웨거 jwt 추가

--- Notechigima/src/main/java/com/capstone/notechigima/config/Swagger2Config.java
+++ Notechigima/src/main/java/com/capstone/notechigima/config/Swagger2Config.java
@@ -1,5 +1,7 @@
 package com.capstone.notechigima.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.GroupedOpenApi;
@@ -8,6 +10,12 @@ import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class Swagger2Config {
 
     @Bean
@@ -20,10 +28,10 @@ public class Swagger2Config {
 
     @Bean
     public OpenAPI springOpenAPI() {
+
         return new OpenAPI()
                 .info(new Info().title("Notechigima API")
                         .description("Notechigima API 명세서입니다.")
                         .version("v1.1.2"));
     }
-
 }
