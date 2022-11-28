e6f1fcfe7d08d1936dfeec6618ed93bc7865d649
김형기 <khk211113@naver.com>
2022-11-29 04:45:09 +0900

    feat: 유저 API 권한 설정

--- Notechigima/src/main/java/com/capstone/notechigima/controller/UserController.java
+++ Notechigima/src/main/java/com/capstone/notechigima/controller/UserController.java
@@ -4,11 +4,13 @@ import com.capstone.notechigima.config.BaseResponse;
 import com.capstone.notechigima.config.SuccessCode;
 import com.capstone.notechigima.dto.invite.GroupInviteGetResponseDTO;
 import com.capstone.notechigima.dto.study_group.StudyGroupGetResponseDTO;
+import com.capstone.notechigima.service.AuthService;
 import com.capstone.notechigima.service.GroupInviteService;
 import com.capstone.notechigima.service.GroupService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.expression.AccessException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,21 +25,27 @@ public class UserController {
 
     private final GroupService groupService;
     private final GroupInviteService groupInviteService;
+    private final AuthService authService;
 
     @ResponseBody
     @GetMapping("/{userId}/groups")
     @Operation(summary = "그룹 조회", description = "사용자가 속한 그룹 목록 조회")
     public BaseResponse<List<StudyGroupGetResponseDTO>> getGroupList(
             @PathVariable("userId") int userId,
-            @RequestHeader(ACCESS_TOKEN_HEADER) String token) {
+            @RequestHeader(ACCESS_TOKEN_HEADER) String token) throws AccessException {
 
+        authService.validateByUserId(token, userId);
         return new BaseResponse(SuccessCode.SUCCESS_READ, groupService.getStudyGroupsByUserId(userId));
     }
 
     @ResponseBody
     @GetMapping("/{userId}/invites")
     @Operation(summary = "초대된 그룹 목록", description = "나한테 그룹 초대를 요청한 목록 조회")
-    public BaseResponse<List<GroupInviteGetResponseDTO>> getGroupInvitedList(@PathVariable("userId") int userId) {
+    public BaseResponse<List<GroupInviteGetResponseDTO>> getGroupInvitedList(
+            @PathVariable("userId") int userId,
+            @RequestHeader(ACCESS_TOKEN_HEADER) String token) throws AccessException {
+
+        authService.validateByUserId(token, userId);
         return new BaseResponse(SuccessCode.SUCCESS_READ, groupInviteService.getGroupInvited(userId));
     }
 }
