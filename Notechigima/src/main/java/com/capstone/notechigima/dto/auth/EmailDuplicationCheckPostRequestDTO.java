1340993560b9a07f47b4013f4b7fcd08b8faa65e
김형기 <khk211113@naver.com>
2022-11-28 15:45:07 +0900

    docs: 스웨거 이메일중복확인 설명 추가

--- Notechigima/src/main/java/com/capstone/notechigima/dto/auth/EmailDuplicationCheckPostRequestDTO.java
+++ Notechigima/src/main/java/com/capstone/notechigima/dto/auth/EmailDuplicationCheckPostRequestDTO.java
@@ -1,5 +1,6 @@
 package com.capstone.notechigima.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class EmailDuplicationCheckPostRequestDTO {
+    @Schema(description = "중복확인할 이메일", defaultValue = "khk211113@naver.com")
     private String email;
 
     @Builder
