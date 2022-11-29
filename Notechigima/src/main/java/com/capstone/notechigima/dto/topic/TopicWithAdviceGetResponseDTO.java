45fa734f76dd98656920002ae27aaac7ff3a93ff
김형기 <khk211113@naver.com>
2022-11-30 03:59:18 +0900

    chore: 분석결과 조회 API 토픽 정보 포함

---
+++ Notechigima/src/main/java/com/capstone/notechigima/dto/topic/TopicWithAdviceGetResponseDTO.java
@@ -0,0 +1,26 @@
+package com.capstone.notechigima.dto.topic;
+
+import com.capstone.notechigima.dto.advice.AdviceGetResponseDTO;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class TopicWithAdviceGetResponseDTO {
+    @Schema(description = "토픽 ID", defaultValue = "23")
+    private int topicId;
+    @Schema(description = "토픽명", defaultValue = "2-3. 프레이밍")
+    private String title;
+    private List<AdviceGetResponseDTO> advices;
+
+    @Builder
+    public TopicWithAdviceGetResponseDTO(int topicId, String title, List<AdviceGetResponseDTO> advices) {
+        this.topicId = topicId;
+        this.title = title;
+        this.advices = advices;
+    }
+}
