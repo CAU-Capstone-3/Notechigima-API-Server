87b7c41c28ebf81ad064c1e5d51c67c82f2d00fc
김형기 <khk211113@naver.com>
2022-11-30 01:20:54 +0900

    fix: UsernameNotFoundException 핸들링

--- Notechigima/src/main/java/com/capstone/notechigima/config/GlobalExceptionHandler.java
+++ Notechigima/src/main/java/com/capstone/notechigima/config/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.capstone.notechigima.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.expression.AccessException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -30,6 +31,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<ErrorResponse> handleAccessException() {
         log.info("handleAccessException throw Exception : {}", ExceptionCode.PERMISSION_DENIED);
         return ErrorResponse.toResponseEntity(ExceptionCode.PERMISSION_DENIED);
+    }
+
+    @ExceptionHandler(value = {UsernameNotFoundException.class})
+    protected ResponseEntity<ErrorResponse> handleUsernamenotFoundException() {
+        log.info("handleUsernameNotFoundException throw Exception : {}", ExceptionCode.ERROR_NOT_FOUND_USER);
+        return ErrorResponse.toResponseEntity(ExceptionCode.ERROR_NOT_FOUND_USER);
     }
 
 }
